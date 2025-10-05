359fa7beccb1b60c0cfc58a676b857e2ea1b0be6
alexmortelsison <alexmortelsison@gmail.com>
2025-10-05 21:01:33 +0900

    updated home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,82 +1,14 @@
 "use client";
-import { TbMenu } from "react-icons/tb";
 import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
-import { BiSearch } from "react-icons/bi";
-import { AiOutlineHeart, AiOutlineShopping } from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
-import Link from "next/link";
 
-const navIcons = [
-  { icon: <BiSearch />, link: "" },
-  { icon: <AiOutlineHeart />, link: "" },
-  { icon: <BsPerson />, link: "" },
-  { icon: <AiOutlineShopping />, link: "" },
-];
-
-export default function Home() {
-  const diorRef = useRef(null);
+export default function HomePage() {
   const bgRef = useRef(null);
-  const menuRef = useRef(null);
-  const iconsRef = useRef(null);
-
-  useEffect(() => {
-    gsap.to(bgRef.current, {
-      y: -300,
-      duration: 0.5,
-      delay: 2,
-      ease: "power2.out",
-    });
-    gsap.to(diorRef.current, {
-      scale: 0.6,
-      delay: 2,
-      duration: 1,
-      ease: "back.out",
-    });
-    gsap.to(menuRef.current, {
-      opacity: 1,
-      delay: 3,
-    });
-    gsap.to(iconsRef.current, {
-      opacity: 1,
-      delay: 3,
-    });
-  });
 
   return (
-    <div className="h-[100vh] w-screen flex">
-      <div className="bg-white min-h-screen w-screen relative">
-        <div
-          className="bg-white min-h-screen w-full flex flex-col justify-center items-center"
-          ref={bgRef}
-        >
-          <div className="flex justify-between items-center w-screen">
-            <div ref={menuRef} className="opacity-0">
-              <TbMenu className="text-2xl" />
-            </div>
-            <h1
-              className="text-7xl text-black font-nicolas tracking-tighter scale-100"
-              ref={diorRef}
-            >
-              DIOR
-            </h1>
-            <div
-              className="flex items-center space-x-4 text-2xl opacity-0"
-              ref={iconsRef}
-            >
-              {navIcons.map((item, index) => (
-                <Link key={index} href={item.link}>
-                  {item.icon}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="w-screen pr-24">
-            <h1 className="text-3xl pt-20 text-center flex justify-center">
-              Spring/Summer 2026 Collection Show
-            </h1>
-          </div>
-        </div>
+    <div className="h-[100vh] w-screen">
+      <div className="flex justify-center items-center h-full">
+        <h1 className="text-6xl font-nicolas scale-100">DIOR</h1>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   const rightVideoRef = useRef<HTMLVideoElement | null>(null);
   const onClickRef1 = useRef(null);
   const onClickRef2 = useRef(null);
+  const diorRef = useRef(null);
   const router = useRouter();
+  const bgRef = useRef(null);
 
   const handleOnClick = () => {
     gsap.to(onClickRef1.current, {
@@ -23,9 +25,22 @@ export default function Home() {
       duration: 1,
       ease: "power2.out",
     });
+    gsap.to(diorRef.current, {
+      scale: 0.6,
+      delay: 0.8,
+      duration: 0.9,
+      color: "#000000",
+      ease: "power2.inOut",
+    });
+    gsap.to(bgRef.current, {
+      y: "100vh",
+      duration: 5,
+      delay: 5,
+      ease: "power2.inOut",
+    });
     setTimeout(() => {
       router.push("/home");
-    }, 2500);
+    }, 1400);
   };
 
   const handleMouseEnterLeft = () => {
@@ -51,62 +66,68 @@ export default function Home() {
     }
   };
   return (
-    <div className="h-[100vh] w-full overflow-hidden">
-      <div className="flex relative inset-0">
-        <div
-          className="relative group"
-          onMouseEnter={() => handleMouseEnterLeft()}
-          onMouseLeave={() => handleMouseLeaveLeft()}
-        >
-          <video
-            loop
-            muted
-            src={"/left.mp4"}
-            className="object-cover w-screen -z-50"
-            ref={leftVideoRef}
-          />
+    <div
+      className="h-full w-screen bg-white z-50 relative inset-0 "
+      ref={bgRef}
+    >
+      <div className="h-[100vh] w-full overflow-hidden absolute inset-0 z-30">
+        <div className="flex relative inset-0">
           <div
-            className="absolute inset-0  flex flex-col justify-center items-center pt-92 gap-y-2"
-            ref={onClickRef1}
+            className="relative group"
+            onMouseEnter={() => handleMouseEnterLeft()}
+            onMouseLeave={() => handleMouseLeaveLeft()}
           >
-            <p className="text-white text-4xl tracking-tight">
-              Fashion & Accessories
-            </p>
-            <p className="text-white tracking-tight underline">Learn more</p>
+            <video
+              loop
+              muted
+              src={"/left.mp4"}
+              className="object-cover w-screen -z-50"
+              ref={leftVideoRef}
+            />
+            <div
+              className="absolute inset-0  flex flex-col justify-center items-center pt-92 gap-y-2"
+              ref={onClickRef1}
+            >
+              <p className="text-white text-4xl tracking-tight">
+                Fashion & Accessories
+              </p>
+              <p className="text-white tracking-tight underline">Learn more</p>
+            </div>
+            <div className="absolute bg-black/50 inset-0 group-hover:opacity-0 transition-opacity duration-300 group-hover:cursor-pointer z-10"></div>
           </div>
-          <div className="absolute bg-black/50 inset-0 group-hover:opacity-0 transition-opacity duration-300 group-hover:cursor-pointer z-10"></div>
-        </div>
-        <div
-          className="relative group"
-          onMouseEnter={() => handleMouseEnterRight()}
-          onMouseLeave={() => handleMouseLeaveRight()}
-        >
-          <video
-            loop
-            muted
-            src={"/right.mp4"}
-            className="object-cover w-screen -z-50"
-            ref={rightVideoRef}
-          />
           <div
-            className="absolute inset-0  flex flex-col justify-center items-center pt-92 gap-y-2"
-            ref={onClickRef2}
+            className="relative group"
+            onMouseEnter={() => handleMouseEnterRight()}
+            onMouseLeave={() => handleMouseLeaveRight()}
           >
-            <p className="text-white text-4xl tracking-tight">
-              Fragrance & Beauty
-            </p>
-            <p className="text-white tracking-tight underline">Learn more</p>
+            <video
+              loop
+              muted
+              src={"/right.mp4"}
+              className="object-cover w-screen -z-50"
+              ref={rightVideoRef}
+            />
+            <div
+              className="absolute inset-0  flex flex-col justify-center items-center pt-92 gap-y-2"
+              ref={onClickRef2}
+            >
+              <p className="text-white text-4xl tracking-tight">
+                Fragrance & Beauty
+              </p>
+              <p className="text-white tracking-tight underline">Learn more</p>
+            </div>
+            <div className="absolute bg-black/50 inset-0 group-hover:opacity-0 transition-opacity duration-300 group-hover:cursor-pointer z-10"></div>
           </div>
-          <div className="absolute bg-black/50 inset-0 group-hover:opacity-0 transition-opacity duration-300 group-hover:cursor-pointer z-10"></div>
         </div>
-      </div>
-      <div className="absolute inset-0 flex justify-center items-center">
-        <h1
-          className="text-white text-8xl font-nicolas tracking-tighter z-50 hover:cursor-pointer scale-100"
-          onClick={() => handleOnClick()}
-        >
-          DIOR
-        </h1>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <h1
+            className="text-white text-8xl font-nicolas tracking-tighter z-50 hover:cursor-pointer scale-100"
+            onClick={() => handleOnClick()}
+            ref={diorRef}
+          >
+            DIOR
+          </h1>
+        </div>
       </div>
     </div>
   );
