6740b32367ab263823168c5e646b7353f8a5f5cd
alexmortelsison <alexmortelsison@gmail.com>
2025-10-06 19:10:17 +0900

    updated footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,40 +5,40 @@ export default function Footer() {
   return (
     <div className="h-[100vh] w-screen bg-white mt-8 overflow-hidden px-8">
       <div className="bg-gray-100 p-8 rounded-lg">
-        <div className="flex justify-between">
+        <div className="flex justify-between border-b pb-12">
           <div className="flex items-center">
             <div>
               <h2>Boutique pick-up</h2>
-              <p>
+              <p className="text-gray-400">
                 Order online and collect your items at your nearest boutique
               </p>
             </div>
             <div className="pl-24 flex items-center">
-              <BiChevronRight className="text-4xl" />
-              <p className="text-2xl pl-8">|</p>
+              <BiChevronRight className="text-4xl text-gray-400" />
+              <p className="text-2xl pl-8 text-gray-400">|</p>
             </div>
           </div>
           <div className="flex items-center">
             <div>
               <h2>Boutique pick-up</h2>
-              <p>
+              <p className="text-gray-400">
                 Order online and collect your items at your nearest boutique
               </p>
             </div>
             <div className="pl-24 flex items-center">
-              <BiChevronRight className="text-4xl" />
-              <p className="text-2xl pl-8">|</p>
+              <BiChevronRight className="text-4xl text-gray-400" />
+              <p className="text-2xl pl-8 text-gray-400">|</p>
             </div>
           </div>
           <div className="flex items-center">
             <div>
               <h2>Boutique pick-up</h2>
-              <p>
+              <p className="text-gray-400">
                 Order online and collect your items at your nearest boutique
               </p>
             </div>
             <div className="pl-24 flex items-center">
-              <BiChevronRight className="text-4xl" />
+              <BiChevronRight className="text-4xl text-gray-400" />
             </div>
           </div>
         </div>
